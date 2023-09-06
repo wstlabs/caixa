65c7cba3e5ed26de6628bd8272879687dff9d9ef
wstlabs <nobody@none.org>
2023-09-06 14:30:27 -0400

    version bump - 0.0.4

--- caixa/__init__.py
+++ caixa/__init__.py
@@ -1,5 +1,5 @@
 """Magic box of helper functions and utility classes"""
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 
 from .argparse import ArgumentParser
 from .xdir import XDir
--- tests/test_caixa.py
+++ tests/test_caixa.py
@@ -1,6 +1,6 @@
 from caixa import __version__
 
 def test_version():
-    assert __version__ == '0.0.3'
+    assert __version__ == '0.0.4'
 
 
